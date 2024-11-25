78e1d2ce76d9309fc73c4ef0a9c9bfaaf825dc2d
danielZerihon <daniel92023@gmail.com>
2024-11-25 17:12:00 +0200

    project run end to end

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "tf_tf_myvpc" {
+resource "aws_vpc" "tf_myvpc" {
   cidr_block = var.cidr
 }
 
@@ -28,13 +28,114 @@ resource "aws_route_table" "tf_route_table" {
   }
 }
 
-resource "aws_route_tabla_association" "tf_route_table_association1" {
+resource "aws_route_table_association" "tf_route_table_association1" {
   subnet_id      = aws_subnet.tf_subnet1.id
   route_table_id = aws_route_table.tf_route_table.id
 }
 
-resource "aws_route_tabla_association" "tf_route_table_association2" {
+resource "aws_route_table_association" "tf_route_table_association2" {
   subnet_id      = aws_subnet.tf_subnet2.id
   route_table_id = aws_route_table.tf_route_table.id
 }
 
+resource "aws_security_group" "tf-security-group" {
+  name        = "web-security-group"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.tf_myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "security_group"
+  }
+}
+
+resource "aws_s3_bucket" "s3-bucket" {
+  bucket = "tf-bucket-vpc-project-daniel-zerihon"
+}
+
+resource "aws_instance" "tf_ec2_subnet1" {
+  ami                    = "ami-0866a3c8686eaeeba"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.tf-security-group.id]
+  subnet_id              = aws_subnet.tf_subnet1.id
+  user_data              = base64encode(file("userdata_subnet1.sh"))
+}
+
+resource "aws_instance" "tf_ec2_subnet2" {
+  ami                    = "ami-0866a3c8686eaeeba"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.tf-security-group.id]
+  subnet_id              = aws_subnet.tf_subnet2.id
+  user_data              = base64encode(file("userdata_subnet2.sh"))
+}
+
+#create alb 
+resource "aws_lb" "myalb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.tf-security-group.id]
+  subnets            = [aws_subnet.tf_subnet1.id, aws_subnet.tf_subnet2.id]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+resource "aws_lb_target_group" "tf_my_alb_target_group" {
+  name     = "targetGroup"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.tf_myvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tf_alb_target_group_attachment_1" {
+  target_group_arn = aws_lb_target_group.tf_my_alb_target_group.arn
+  target_id        = aws_instance.tf_ec2_subnet1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tf_alb_target_group_attachment_2" {
+  target_group_arn = aws_lb_target_group.tf_my_alb_target_group.arn
+  target_id        = aws_instance.tf_ec2_subnet2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "tf_listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.tf_my_alb_target_group.arn
+    type             = "forward"
+  }
+}
+
+output "loadbancerdns" {
+  value = aws_lb.myalb.dns_name
+}
